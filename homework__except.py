f38f59ce8999b8c8d987ecf3e75a3eab3fd04fd4
Dianas92 <45029687+Dianas92@users.noreply.github.com>
2018-11-27 19:35:02 +0400

    Update homework__except.py

--- homework__except.py
+++ homework__except.py
@@ -2,12 +2,13 @@ a = input('Введите операцию :')
 b=a.strip().split(' ')
 try:
  operand_1 = int(b[1])
- operand_2 = int(b[2]) 
+ operand_2 = int(b[2])
+ assert (operand_1 >= 0) & (operand_2 >=0) 
 except:
- print(' операнды должен быть числом')
+ print('Введено некорректное выражение')
 else:
- result="Введено некорректное выражение"
- assert (operand_1 >= 0) & (operand_2 >=0),'Нужно вводить положительные числа'
+ result= 'Невозможно вычислить'
+ 
  if b[0] =='+':
    result = operand_1+operand_2 
  elif b[0] =="-":
