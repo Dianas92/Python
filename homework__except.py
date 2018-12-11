4dc38f61fb7feae1b4efcf0b804a79a82238e778
Dianas92 <45029687+Dianas92@users.noreply.github.com>
2018-12-11 20:15:14 +0400

    Update homework__except.py

--- homework__except.py
+++ homework__except.py
@@ -1,21 +1,30 @@
-a = input('Введите операцию :')
-b=a.strip().split(' ')
-try:
- operand_1 = int(b[1])
- operand_2 = int(b[2])
- assert (operand_1 >= 0) & (operand_2 >=0) 
-except:
- print('Введено некорректное выражение')
-else:
- result= 'Невозможно вычислить'
- 
- if b[0] =='+':
-   result = operand_1+operand_2 
- elif b[0] =="-":
-   result = operand_1-operand_2 
- elif b[0] =="*":
-   result = operand_1*operand_2 
- elif b[0] =="/":
-   result =operand_1/operand_2 
+error_code=1
+while error_code !=0:
 
- print(result)
+ a = input('Введите операцию :')
+ b=a.strip().split(' ')
+ try:
+   operand_1 = int(b[1])
+   operand_2 = int(b[2])
+   assert (operand_1 >= 0) & (operand_2 >=0) 
+ except AssertionError:
+   print('Числа должны быть положительными')
+ except:
+   print('Введены некорректные данные')
+ else:
+   error_code=0
+ if b[0] !=('+' or '-' or '*' or '/'):
+   error_code=1
+   print('Некорректно введен знак операции')
+
+ 
+if b[0] =='+':
+  result = operand_1+operand_2 
+elif b[0] =="-":
+  result = operand_1-operand_2 
+elif b[0] =="*":
+  result = operand_1*operand_2 
+elif b[0] =="/":
+  result =operand_1/operand_2 
+
+print(result)
