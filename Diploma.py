c676f64ea992157d9200fd041fcdeeaad45deaf5
Dianas92 <45029687+Dianas92@users.noreply.github.com>
2018-12-26 21:12:44 +0400

    Update Diploma.py

--- Diploma.py
+++ Diploma.py
@@ -1,8 +1,13 @@
 import requests
+import os
+import json
 from pprint import pprint
 
 token = 'ed1271af9e8883f7a7c2cefbfddfcbc61563029666c487b2f71a5227cce0d1b533c4af4c5b888633c06ae'
-params = {'access_token':token,'user_id': '171691064', 'v': '5.92'}
+user_id = '171691064'
+print('Получаем список друзей пользователя')
+
+params = {'access_token':token,'user_id': user_id, 'v': '5.92'}
 
 friend_list = requests.get('https://api.vk.com/method/friends.get', params).json()['response']['items']
 
@@ -10,20 +15,35 @@ friends=''
 for number in friend_list:
   friends = friends + str(number) + ','
 
-#print(friends)
-print('')
 
+print('Получаем список групп пользователя')
 params = {'access_token':token,'user_id': '171691064', 'v': '5.92'}
 group_list = requests.get('https://api.vk.com/method/groups.get', params).json()['response']['items']
-#print(group_list)
-print('')
+
+print('Обработка результатов')
+
+records = []
+
 
 for number in group_list:
   params = {'group_id': number, 'access_token': token,'user_ids': friends, 'v': '5.92'}
-  answer = requests.get('https://api.vk.com/method/groups.isMember', params).json()['response']
+  answer = requests.get('https://api.vk.com/method/groups.isMember', params).json()['response'] 
+ 
+  params = {'access_token':token,'group_id': number, 'v': '5.92'}
+  group_name = requests.get('https://api.vk.com/method/groups.getById', params).json()['response'][0]['name']
   
-  print('В группе '+str(number)+' состоят:')
+  
+  print('.')
+  users_in_group = 0
   for line in answer:
     if line['member'] == 1:
-      print(line['user_id'])
+      users_in_group += 1
+ 
+  if users_in_group == 0:
+    params = {'access_token':token,'group_id': number, 'fields':'members_count', 'v': '5.92'}
+    members_count = requests.get('https://api.vk.com/method/groups.getById', params).json()['response'][0]['members_count']
+    records.append({'name':group_name,'gid':number,'members_count':members_count})
 
+with open('groups.json', 'w') as outfile:
+  json.dump(records, outfile)
+print('Программа завершила работу, результаты можно увидеть в файле groups.json')
